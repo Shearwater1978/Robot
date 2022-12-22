5f762b65ff13f245b85e7df4c452df184620b378
uglykoyote <uglykoyote@gmail.com>
2022-12-22 16:42:11 +0100

    Added some functionalty for support passing test URL via env variable

--- aux_scripts/selenium_test.py
+++ aux_scripts/selenium_test.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 
 options = webdriver.ChromeOptions()
@@ -9,9 +10,9 @@ options.add_argument('--start-maximized')
 options.add_argument('--user-data-dir=~/.config/google-chrome')
 
 driver = webdriver.Chrome('/opt/chromedriver',options=options, service_args=["--verbose", "--log-path=./log.log"])
-driver.save_screenshot("./image.png")
 
-url = 'https://www.google.com'
+url = os.environ.get('TEST_URL', 'https://www.google.com')
 driver.get(url)
+driver.save_screenshot("/opt/screenshots/image.png")
 
 driver.close()
