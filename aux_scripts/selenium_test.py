ab17334964a678ffa143106c95f5f87c985a43f4
Uglykoyote <uglykoyote@yandex.ru>
2022-12-22 16:03:55 +0100

    Update selenium_test.py

--- aux_scripts/selenium_test.py
+++ aux_scripts/selenium_test.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver import DesiredCapabilities
 
 options = webdriver.ChromeOptions()
 options.binary_location = '/opt/chrome/chrome'
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--headless')
+options.add_argument('--start-maximized')
+options.add_argument('--user-data-dir=~/.config/google-chrome')
 
-driver = webdriver.Chrome('/opt/chromedriver',chrome_options=options)
+driver = webdriver.Chrome('/opt/chromedriver',options=options, service_args=["--verbose", "--log-path=./log.log"])
+driver.save_screenshot("./image.png")
 
-driver.get('https://www.google.com/')
+url = 'https://www.google.com'
+driver.get(url)
 
-driver.close();
-driver.quit();
+driver.close()
